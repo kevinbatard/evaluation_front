c63184940113c7be383b86793861ffac6a4933cf
kevinbatard <kevin.batard4064@gmail.com>
2023-03-20 14:21:24 +0100

    feat header

---
+++ src/components/Header.tsx
@@ -0,0 +1,7 @@
+export default function Header() {
+    return (
+        <header className="bg-primary header">
+            <h1 className="text-start font-light p-3"> Produits </h1>
+        </header>
+    );
+}
